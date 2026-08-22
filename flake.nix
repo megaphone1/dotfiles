ecf968d600cce71d06ad532a24c39392b178599f
Anonymous <anon@devnull.com>
2026-08-22 12:49:26 -0600

    Move cachix into module

--- flake.nix
+++ flake.nix
@@ -1,22 +1,4 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://noctalia.cachix.org"
-      "https://ghostty.cachix.org"
-    ];
-
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      "ghostty.cachix.org-1:QB389yTa6GTyneehvqG58Y0WnHjQOqgnA+wBnpWWxns="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
--- modules/features/cachix.nix
+++ modules/features/cachix.nix
@@ -9,6 +9,13 @@
             "@wheel"
             "${config.user.name}"
           ];
+
+          extra-trusted-users = [
+            "root"
+            "@wheel"
+            "${config.user.name}"
+          ];
+
           substituters = [
             "https://cache.nixos.org/"
             "https://nix-community.cachix.org"
@@ -17,7 +24,23 @@
             "https://ghostty.cachix.org"
           ];
 
+          extra-substituters = [
+            "https://cache.nixos.org/"
+            "https://nix-community.cachix.org"
+            "https://hyprland.cachix.org"
+            "https://noctalia.cachix.org"
+            "https://ghostty.cachix.org"
+          ];
+
           trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+            "ghostty.cachix.org-1:QB389yTa6GTyneehvqG58Y0WnHjQOqgnA+wBnpWWxns="
+          ];
+
+          extra-trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
