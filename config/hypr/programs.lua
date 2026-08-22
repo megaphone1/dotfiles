f6dacee428cf2d9ad306485ed54336ccdc12be57
Anonymous <anon@devnull.com>
2026-08-22 13:34:46 -0600

    Use XDG directory for org-roam files

--- config/hypr/programs.lua
+++ config/hypr/programs.lua
@@ -5,8 +5,7 @@
 -- local editor = 'emacsclient -c -a ""'
 local editor = "emacs"
 local browser = "firefox"
--- local terminal = "ghostty"
-local terminal = "GDK_DEBUG=vulkan-disable ghostty"
+local terminal = "alacritty"
 
 local launcherMenu = "noctalia msg panel-toggle launcher"
 local launcherEmoji = "noctalia msg panel-toggle launcher /emo"
