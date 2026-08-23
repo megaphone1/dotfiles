26bb89033784c06fb1b7f53baea2fe80dde0da1a
Anonymous <anon@devnull.com>
2026-08-22 20:54:20 -0600

    BUGFIX! audi working on qemu host

--- modules/features/pipewire.nix
+++ modules/features/pipewire.nix
@@ -1,17 +1,21 @@
 { ... }:
 {
-  flake.nixosModules.pipewire = {
-    services.pulseaudio.enable = false;
-
+  flake.nixosModules.pipewire = { config, ... }: {
     security.rtkit.enable = true;
 
     services.pipewire = {
       enable = true;
+      systemWide = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
       wireplumber.enable = true;
     };
+
+    users.users.${config.user.name}.extraGroups = [
+      "audio"
+      "pipewire"
+    ];
   };
 }
