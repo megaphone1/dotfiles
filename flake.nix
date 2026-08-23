169e01d3cf5a92a071a4ea808abe78124ecc9be5
Anonymous <anon@devnull.com>
2026-08-22 21:35:17 -0600

    Add nur cachix

--- flake.nix
+++ flake.nix
@@ -6,8 +6,9 @@
     ];
 
     extra-substituters = [
-      "https://cache.nixos.org/"
+      "https://cache.nixos.org"
       "https://nix-community.cachix.org"
+      "https://nur.cachix.org"
       "https://hyprland.cachix.org"
       "https://noctalia.cachix.org"
       "https://ghostty.cachix.org"
@@ -17,6 +18,7 @@
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nur.cachix.org-1:F8+2oprcHLfsYyZBCsVJZJrPyGHwuE+EZBtukwalV7o="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "ghostty.cachix.org-1:QB389yTa6GTyneehvqG58Y0WnHjQOqgnA+wBnpWWxns="
@@ -32,6 +34,9 @@
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nur.url = "github:nix-community/NUR";
+    nur.inputs.nixpkgs.follows = "nixpkgs";
 
     stylix.url = "github:nix-community/stylix";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
