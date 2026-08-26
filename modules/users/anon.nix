59380c822178a4217fc947c8b441676063756ac4
Anonymous <anon@devnull.com>
2026-08-25 18:51:07 -0600

    Enable fetch

--- modules/users/anon.nix
+++ modules/users/anon.nix
@@ -12,6 +12,7 @@
       self.homeModules.noctalia
       self.homeModules.spicetify
       self.homeModules.golang
+      self.homeModules.fetch
       self.homeModules.yazi
       self.homeModules.zen
     ];
