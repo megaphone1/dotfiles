28d98663a1fbe37d0cf54dd17ecb4ed3f7d2145f
Anonymous <anon@devnull.com>
2026-09-06 10:31:06 -0600

    Change default names

--- modules/features/emacs/fonts.nix
+++ modules/features/emacs/fonts.nix
@@ -1,76 +1,70 @@
-{ ... }:
-{
-  flake.homeModules.emacsFonts =
-    {
-      lib,
-      pkgs,
-      config,
-      ...
-    }:
-    let
-      cfg = config.emacs;
-    in
-    {
-      config = lib.mkIf cfg.enable {
-        fonts.fontconfig.enable = true;
+{...}: {
+  flake.homeModules.emacsFonts = {
+    lib,
+    pkgs,
+    config,
+    ...
+  }: let
+    cfg = config.emacs;
+  in {
+    config = lib.mkIf cfg.enable {
+      fonts.fontconfig.enable = true;
 
-        emacs = {
-          extraPackages = with pkgs; [
-            fira-code-symbols
-            emacs-all-the-icons-fonts
-          ];
+      emacs = {
+        extraPackages = with pkgs; [
+          fira-code-symbols
+          emacs-all-the-icons-fonts
+        ];
 
-          extraEmacsPackages = with pkgs.emacsPackages; [
-            emojify
-            ligature
-            nerd-icons
-            all-the-icons
-            all-the-icons-dired
-            all-the-icons-ivy-rich
-          ];
+        extraEmacsPackages = with pkgs.emacsPackages; [
+          emojify
+          ligature
+          nerd-icons
+          all-the-icons
+          all-the-icons-dired
+          all-the-icons-ivy-rich
+        ];
 
-          init = ''
-            (add-hook 'after-init-hook 'global-emojify-mode)
+        extraInit = ''
+          (add-hook 'after-init-hook 'global-emojify-mode)
 
-            (ligature-set-ligatures 't '("www"))
-            (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
-            (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
-                                                 ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
-                                                 "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
-                                                 "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
-                                                 "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
-                                                 "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
-                                                 "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
-                                                 "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
-                                                 ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
-                                                 "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
-                                                 "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
-                                                 "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
-                                                 "\\\\" "://"))
+          (ligature-set-ligatures 't '("www"))
+          (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+          (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                               ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                               "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                               "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                               "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                               "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                               "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                               "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                               ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                               "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                               "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                               "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                               "\\\\" "://"))
 
-            (global-ligature-mode t)
-            (global-prettify-symbols-mode +1)
+          (global-ligature-mode t)
+          (global-prettify-symbols-mode +1)
 
-            (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
-            (setq all-the-icons-dired-monochrome nil)
-            (all-the-icons-ivy-rich-mode +1)
+          (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+          (setq all-the-icons-dired-monochrome nil)
+          (all-the-icons-ivy-rich-mode +1)
 
-          '';
+        '';
 
-          keys = {
-            macro = ''
-              (defhydra hydra-text-scale (:timeout 4)
-                "Scale the text in the current buffer."
-                ("k" text-scale-decrease "Decrease")
-                ("j" text-scale-increase "Increase")
-                ("f" nil "Finished" :exit t))
-            '';
+        extraMacros = ''
+          (defhydra hydra-text-scale (:timeout 4)
+            "Scale the text in the current buffer."
+            ("k" text-scale-decrease "Decrease")
+            ("j" text-scale-increase "Increase")
+            ("f" nil "Finished" :exit t))
+        '';
 
-            bind = ''
-              "tf" '(hydra-text-scale/body :which-key "Font")
-            '';
-          };
-        };
+        extraBinds = ''
+          "tf" '(hydra-text-scale/body :which-key "Font")
+        '';
       };
     };
+  };
 }
